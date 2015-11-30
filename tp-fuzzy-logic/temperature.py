1064128a0d8cb4396640bc165303a4415d0a8fe1
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-11-30 20:57:13 +0100

    fonction mamdani terminée

--- tp-fuzzy-logic/temperature.py
+++ tp-fuzzy-logic/temperature.py
@@ -11,6 +11,10 @@ temp = range(minT, maxT+1, step)
 
 
 def low(t):
+    """
+    Fonction qui descrit les températures basses.
+    @return facteur de température basse en fonction de la température t.
+    """
     if (t <= 10):
         # low
         return 1.0
@@ -22,6 +26,10 @@ def low(t):
         return 0.0
 
 def medium(t):
+    """
+    Fonction qui descrit les températures moyennes.
+    @return facteur de température moyenne en fonction de la température t.
+    """
     if (t <= 10 or t >= 30):
         # not medium
         return 0.0
@@ -34,6 +42,10 @@ def medium(t):
 
 
 def high(t):
+    """
+    Fonction qui descrit les températures hautes.
+    @return facteur de température haute en fonction de la température t.
+    """
     if (t <= 20):
         # not high
         return 0.0
@@ -45,6 +57,10 @@ def high(t):
         return 1.0
 
 def heatStrongly(p):
+    """
+    Fonction qui descrit si nous devons chauffer fort.
+    @return facteur de "chauffer fort" en fonction de la puissance p.
+    """
     if (p <= 8):
         # not strongly
         return 0.0
@@ -55,12 +71,44 @@ def heatStrongly(p):
         # strongly
         return 1.0
 
+def solveHeatStrongly(y):
+    """
+    Resolution de la fonction affine heatStrongly(p).
+    @return puissance chauffe (KW) en fonction du facteur de "chauffer fort" y.
+    """
+    if (y<0.0 or y>1.0):
+        raise ValueError("La fonction ne peut varier que entre 0.0 et 1.0 sur l'axe des y.")
+    return (y+4) * 2
+
 def mmin(f1, f2, t):
+    """
+    Fonction min entre deux fonctions
+    """
     return min(f1(t), f2(t))
 
 def mmax(f1, f2, t):
+    """
+    Fonction max entre deux fonctions
+    """
     return max(f1(t), f2(t))
 
+def mamdani(t, p):
+    """
+    @param t temperature doit être fix lors des itération
+    @param p puissance varie entre 0 et 15
+    """
+    factor = low(t)
+    powerMax = solveHeatStrongly(factor)
+    if (p >= powerMax):
+        return factor
+    else:
+        return heatStrongly(p)
+
+
+
+
+# MAIN
+# ====
 
 def main():
     lowArray = []
@@ -70,6 +118,7 @@ def main():
     mmaxMH = []
 
     heatArray = []
+    mamdaniArray = []
 
     for t in temp:
         lowArray.append(low(t))
@@ -78,8 +127,9 @@ def main():
         mminLM.append(mmin(low, medium, t))
         mmaxMH.append(mmax(medium, high, t))
 
-    for p in range(0,15,1):
+    for p in range(0, 16, 1):
         heatArray.append(heatStrongly(p))
+        mamdaniArray.append(mamdani(12,p));
 
     #plt.plot(lowArray, marker=".", label="Basse")
     #plt.plot(mediumArray, marker=".", label="Moyenne")
@@ -92,7 +142,9 @@ def main():
     plt.legend()
     plt.show()
 
-    plt.plot(heatArray, marker=".", label="Chauffer fort", color="red")
+    plt.plot(mamdaniArray, marker=".", label="Chauffer fort", color="red")
+    plt.xlabel('Puissance chauffe (KW)')
+    plt.ylabel('Chauffe fort')
     plt.axis([0, 15, -0.1, 1.1])
     plt.legend()
     plt.show()
